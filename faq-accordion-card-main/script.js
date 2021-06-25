34db51b48e0423c51cf65727b92441710dd16127
denalves <50816778+denalves@users.noreply.github.com>
2021-06-25 15:45:19 -0300

    novo template

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -6,10 +6,12 @@ for (i = 0; i < len; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
+    if (panel.style.maxHeight === '0px') {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
+
+    
   });
 }
